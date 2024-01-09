f437a8ece13b959ee113f03e2b8e0539b4c000bd
markmarkmadara <72902563+markmarkmadara@users.noreply.github.com>
2024-01-10 07:02:28 +0800

    login + with sample designs

---
+++ main.py
@@ -0,0 +1,7 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    #Remove debug=True when deploying to production
+    app.run(debug=True)
---
+++ website/views.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from werkzeug.utils import secure_filename
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html", user=current_user)
+
+@views.route('/result')
+@login_required
+def results():
+    return render_template("result.html", user=current_user)
+
+@views.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template("dashboard.html", user=current_user)
+
+@views.route('/submit_sample', methods=['GET', 'POST'])
+@login_required
+def submit_sample():
+    return render_template("submit_sample.html", user=current_user)
+
