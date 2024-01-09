01e1080e3dd71e672e4f5b047c6e2d39410f4d55
Tani Sandoval <tanisandoval02@gmail.com>
2024-01-10 05:59:43 +0800

    Update .gitignore and README.md

---
+++ website/auth.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/signup")
+def register():
+    return "<p>Signup</p>"
+
+
+@auth.route("/login")
+def login():
+    return "<p>Login</p>"
+
+
+@auth.route("/logout")
+def logout():
+    return "<p>Logout</p>"
