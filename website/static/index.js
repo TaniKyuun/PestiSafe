299f3a20fc0548d07bd44bdcdcc56942b2c75552
Tani Sandoval <tanisandoval02@gmail.com>
2024-03-09 01:08:26 +0800

    Refactor index.js: Update variable declarations and remove comments

--- website/static/index.js
+++ website/static/index.js
@@ -12,20 +12,17 @@ $('a[href^="#"]').on("click", function (event) {
   }
 });
 
-let header = document.getElementById("myHeader");
+const header = document.getElementById("myHeader");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
     header.style.backgroundColor = "#1c5d34";
   } else {
-    // Modify the header when at the top
     header.style.backgroundColor = "rgba(174, 246, 148, 0.15)";
   }
 });
 
-// Flash message Close Event
-// Get all close buttons
-let closebtns = document.getElementsByClassName("close");
+const closebtns = document.getElementsByClassName("close");
 
 // Loop through all close buttons
 for (var i = 0; i < closebtns.length; i++) {
