9646c0d81fdeb9ae94547b09c40c3c705e579312
Tani Sandoval <tanisandoval02@gmail.com>
2024-03-09 01:38:50 +0800

    Refactor code formatting

--- website/__init__.py
+++ website/__init__.py
@@ -8,36 +8,36 @@ DB_NAME = "database.db"
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "pestikey"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-    db.init_app(app)
+  app = Flask(__name__)
+  app.config["SECRET_KEY"] = "pestikey"
+  app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+  db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+  from .views import views
+  from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/")
+  app.register_blueprint(views, url_prefix="/")
+  app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User
+  from .models import User
 
-    with app.app_context():
-        db.create_all()
+  with app.app_context():
+    db.create_all()
 
-    # Import the login manager from flask_login
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
+  # Import the login manager from flask_login
+  login_manager = LoginManager()
+  login_manager.login_view = "auth.login"
+  login_manager.init_app(app)
 
-    @login_manager.user_loader
-    # Create a function that will load the user's information
-    def load_user(id):
-        return User.query.get(int(id))
+  @login_manager.user_loader
+  # Create a function that will load the user's information
+  def load_user(id):
+    return User.query.get(int(id))
 
-    return app
+  return app
 
 
 def create_database(app):
-    if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
-        print("Created Database!")
+  if not path.exists("website/" + DB_NAME):
+    db.create_all(app=app)
+    print("Created Database!")
--- website/models.py
+++ website/models.py
@@ -4,21 +4,21 @@ from sqlalchemy.sql import func
 
 
 class Results(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    # Add a new column to the Results table called 'user_id' that will be a relationship to the User table
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+  id = db.Column(db.Integer, primary_key=True)
+  data = db.Column(db.String(10000))
+  date = db.Column(db.DateTime(timezone=True), default=func.now())
+  # Add a new column to the Results table called 'user_id' that will be a relationship to the User table
+  user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(
-        db.Integer, primary_key=True
-    )  # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(150), unique=True)
-    first_name = db.Column(db.String(150))
-    last_name = db.Column(db.String(150))
-    is_active = db.Column(db.Boolean, default=True)
-    password = db.Column(db.String(150))
-    results = db.relationship("Results")
-    # Add a new column to the User table called 'posts' that will be a relationship to the Post table
+  id = db.Column(
+    db.Integer, primary_key=True
+  )  # primary keys are required by SQLAlchemy
+  email = db.Column(db.String(150), unique=True)
+  first_name = db.Column(db.String(150))
+  last_name = db.Column(db.String(150))
+  is_active = db.Column(db.Boolean, default=True)
+  password = db.Column(db.String(150))
+  results = db.relationship("Results")
+  # Add a new column to the User table called 'posts' that will be a relationship to the Post table
