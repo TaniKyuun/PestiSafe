d87acc5ec44f82b8c514da596e1e3f7ba039002a
Tani Sandoval <tanisandoval02@gmail.com>
2024-05-12 05:25:19 +0800

    chore: Update smooth scrolling behavior and add profile picture toggle

--- app/static/index.js
+++ app/static/index.js
@@ -1,19 +1,26 @@
-$('a[href^="#"]').on("click", function (event) {
-  const target = $(this.getAttribute("href"));
-  if (target.length) {
-    event.preventDefault();
-    $("html, body").stop().animate(
-      {
-        scrollTop: target.offset().top,
-      },
-      1000,
-      "easeInOutExpo",
-    );
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        1000,
+        "easeInOutExpo"
+      );
+      return false;
+    }
   }
 });
 
+// Change header background color on scroll
 const header = document.getElementById("myHeader");
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 100) {
     header.style.backgroundColor = "#1c5d34";
@@ -22,11 +29,25 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// Add click event to close buttons
 const closebtns = document.getElementsByClassName("close");
-
 for (let i = 0; i < closebtns.length; i++) {
   closebtns[i].addEventListener("click", function () {
-    // Hide the flash message
     this.parentElement.style.display = "none";
   });
 }
+
+// Toggle profile selection on profile picture click
+document
+  .getElementById("profile-picture")
+  .addEventListener("click", function () {
+    var profileSelection = document.getElementById("profile-selection");
+    if (
+      profileSelection.style.display === "none" ||
+      profileSelection.style.display === ""
+    ) {
+      profileSelection.style.display = "block";
+    } else {
+      profileSelection.style.display = "none";
+    }
+  });
